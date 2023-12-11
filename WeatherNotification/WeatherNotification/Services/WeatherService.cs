20aeeedb39621d2f829a4f35c27906d05af796e9
thecodeprofessor <nathan@abourbih.com>
2023-12-11 14:09:23 -0500

    Created a weather service to centralize all of the calls to the api.

---
+++ WeatherNotification/WeatherNotification/Services/WeatherService.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WeatherNotification.Weather_MVVM.Models;
+
+namespace WeatherNotification.Services
+{
+    public class WeatherService
+    {
+        private readonly HttpClient _httpClient;
+
+        private static WeatherService _instance;
+        public static WeatherService Instance => _instance ??= new WeatherService();
+
+
+        public WeatherService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<WeatherAppData> GetWeatherAsync(string address)
+        {
+            var location = await GetLocationAsync(address);
+            return await GetWeatherAsync(location);
+        }
+
+            public async Task<WeatherAppData> GetWeatherAsync(Location location)
+        {
+            try
+            {
+                if (location == null)
+                    return null;
+
+                var url =
+                    $"https://api.open-meteo.com/v1/forecast?latitude={location.Latitude}&longitude={location.Longitude}&current=temperature_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=America%2FChicago";
+                var response =
+                     await _httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    using var responseStream = await response.Content.ReadAsStreamAsync();
+                    var data = await JsonSerializer
+                         .DeserializeAsync<WeatherAppData>(responseStream);
+
+                    return data;
+                }
+            }
+            catch
+            {
+                //Failed to get data.
+            }
+            return null;
+        }
+
+        private async Task<Location> GetLocationAsync(string address)
+        {
+            try
+            {
+                if (address == null)
+                    return null;
+
+                IEnumerable<Location> locations = await Geocoding
+                     .Default.GetLocationsAsync(address);
+
+                Location location = locations?.FirstOrDefault();
+
+                if (location != null)
+                    Console
+                         .WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                return location;
+            }
+            catch
+            {
+
+            }
+            return null;
+        }
+    }
+
+}
--- WeatherNotification/WeatherNotification/Weather_MVVM/ViewModels/WeatherAppViewModel.cs
+++ WeatherNotification/WeatherNotification/Weather_MVVM/ViewModels/WeatherAppViewModel.cs
@@ -16,7 +16,9 @@ namespace WeatherNotification.Weather_MVVM.ViewModels
     [AddINotifyPropertyChangedInterface]
     public class WeatherViewModel
     {
-        
+
+        protected static Services.WeatherService WeatherService => Services.WeatherService.Instance;
+
         public WeatherAppData WeatherAppData { get; set; }
         private readonly HttpClient _httpClient;
         public string PlaceName { get; set; }
@@ -29,38 +31,10 @@ namespace WeatherNotification.Weather_MVVM.ViewModels
             new Command(async (searchText) =>
             {
                 PlaceName = searchText.ToString();
-                var location = await GetCoordinatesAsync(searchText.ToString());
-                await GetWeather(location);
+                WeatherAppData = await WeatherService.GetWeatherAsync(searchText.ToString());
             });
 
-        private async Task GetWeather(Location location)
-        {
-            var url =
-                $"https://api.open-meteo.com/v1/forecast?latitude={location.Latitude}&longitude={location.Longitude}&current=temperature_2m,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=America%2FChicago";
-            var response =
-                 await _httpClient.GetAsync(url);
 
-            if (response.IsSuccessStatusCode)
-            {
-                using var responseStream = await response.Content.ReadAsStreamAsync();
-                var data = await JsonSerializer
-                     .DeserializeAsync<WeatherAppData>(responseStream);
-                WeatherAppData = data;
-            }
-        }
-        private async Task<Location> GetCoordinatesAsync(string address)
-        {
-            IEnumerable<Location> locations = await Geocoding
-                 .Default.GetLocationsAsync(address);
 
-            Location location = locations?.FirstOrDefault();
-
-            if (location != null)
-                Console
-                     .WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
-            return location;
-        
-
-        }
     }
 }
